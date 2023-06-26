fa2ee14bafa953acc78b7943719e40ab4d0c81bd
Kalash Singhal <kalash@2degrees-investing.org>
2023-06-26 14:01:19 +0200

    setup commit

---
+++ R/tiltIndicatorAfter-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr distinct
+#' @importFrom tiltIndicator xstr_polish_output_at_company_level
+#' @importFrom tiltIndicator unnest_company
+## usethis namespace: end
+NULL
