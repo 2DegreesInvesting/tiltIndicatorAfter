e13a68b8b2e63534f9be7afa3a91f973a94fa5a2
Kalash Singhal <kalash@2degrees-investing.org>
2023-06-27 13:41:58 +0200

    remove warnings

--- R/tiltIndicatorAfter-package.R
+++ R/tiltIndicatorAfter-package.R
@@ -6,5 +6,9 @@
 #' @importFrom tiltIndicator xstr_polish_output_at_company_level
 #' @importFrom tiltIndicator unnest_company
 #' @importFrom dplyr select
+#' @importFrom readr read_csv
+#' @importFrom tibble tibble
+#' @importFrom tidyr nest
+#' @importFrom utils hasName
 ## usethis namespace: end
 NULL
