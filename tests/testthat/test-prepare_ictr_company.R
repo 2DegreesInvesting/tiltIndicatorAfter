6aed2d4f6751d3a5ba68945fe0aaf094008ff791
Kalash Singhal <kalash@2degrees-investing.org>
2023-07-08 11:17:29 +0200

    change tests

--- tests/testthat/test-prepare_ictr_company.R
+++ tests/testthat/test-prepare_ictr_company.R
@@ -1,5 +1,5 @@
 test_that("total number of rows for a comapny is either 1 or 18", {
-  out <- prepare_ictr_company(ictr_company, ictr_product, companies, ecoinvent_activities, matches_mapper, ecoinvent_inputs) |>
+  out <- prepare_ictr_company(ictr_company, ictr_product, ep_companies, ecoinvent_activities, matches_mapper, ecoinvent_inputs) |>
     group_by(companies_id) |>
     summarise(count=n())
   expect_true(all(unique(out$count) %in% c(18, 1)))
