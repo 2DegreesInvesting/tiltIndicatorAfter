92d19573e0a1d7080ded368e23bca47adc42608a
Kalash Singhal <kalash@2degrees-investing.org>
2023-07-13 14:18:13 +0200

    update r scripts

--- R/tiltIndicatorAfter_runner_istr.R
+++ R/tiltIndicatorAfter_runner_istr.R
@@ -50,33 +50,35 @@
 #   mutate(data, done_product = file_exists(file_product), done_company = file_exists(file_company))
 # }
 #
-# istr_comp <- read_csv("/path/to/input/istr_companies.csv") |>
+# istr_comp <- read_csv("/Users/kalashsinghal/git/2DII/tiltIndicatorAfter/vignettes/articles/test/istr_companies.csv") |>
 #   rename(company_id = "companies_id") |>
 #   xstr_prune_companies()
 #
-# istr_inputs_raw <- read_csv("/path/to/input/istr_products_v2.csv", col_types = cols(input_isic_4digit = col_character())) |>
+# istr_inputs_raw <- read_csv("/Users/kalashsinghal/git/2DII/tiltIndicatorAfter/vignettes/articles/test/istr_products.csv", col_types = cols(input_isic_4digit = col_character())) |>
 #   rename(weo_product = "input_weo_sector", weo_flow = "input_weo_subsector", ipr_sector = "input_ipr_sector", ipr_subsector = "input_ipr_subsector") |>
 #   xstr_pivot_type_sector_subsector()
 #
 # scenario_years <- c(2030, 2050)
-# ipr <- read_csv("/path/to/input/str_ipr_targets.csv")
-# weo <- read_csv("/path/to/input/str_weo_targets.csv")
+# ipr <- read_csv("/Users/kalashsinghal/git/2DII/tiltIndicatorAfter/vignettes/articles/test/str_ipr_targets.csv") |>
+#   distinct(scenario, year, ipr_sector, ipr_subsector, .keep_all = TRUE)
+# weo <- read_csv("/Users/kalashsinghal/git/2DII/tiltIndicatorAfter/vignettes/articles/test/str_weo_targets.csv") |>
+#   distinct(scenario, year, weo_sector, weo_subsector, .keep_all = TRUE)
 # scenarios_new <- list(ipr = ipr, weo = weo) |>
 #   xstr_prepare_scenario() |>
 #   filter(year %in% scenario_years)
 #
-# ep_campanies_mapper <- read_csv("/path/to/input/ep_companies_2.csv") |>
+# ep_campanies_mapper <- read_csv("/Users/kalashsinghal/git/2DII/tiltIndicatorAfter/vignettes/articles/test/ep_companies.csv") |>
 #   select("company_name", "country", "company_city", "postcode", "address", "main_activity", "companies_id") |>
 #   distinct()
 #
 # # ecoinvent_activities
-# eco_act <- read_csv("/path/to/input/ei_activities_overview_2.csv")
+# eco_act <- read_csv("/Users/kalashsinghal/git/2DII/tiltIndicatorAfter/vignettes/articles/test/ei_activities_overview.csv")
 #
 # # matches_mapper
-# match_map <- read_csv("/path/to/input/mapper_ep_ei.csv")
+# match_map <- read_csv("/Users/kalashsinghal/git/2DII/tiltIndicatorAfter/vignettes/articles/test/mapper_ep_ei.csv")
 #
 # # ecoinvent_inputs
-# eco_inputs_raw <- read_csv("/path/to/input/ei_input_data_2.csv") |>
+# eco_inputs_raw <- read_csv("/Users/kalashsinghal/git/2DII/tiltIndicatorAfter/vignettes/articles/test/ei_input_data.csv") |>
 #   select("input_activity_uuid_product_uuid", "exchange_name", "exchange_unit_name") |>
 #   distinct()
 #
