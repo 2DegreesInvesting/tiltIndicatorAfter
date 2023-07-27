0b1f2535cd79e51a4f159b2437e292b373b8ae22
Kalash Singhal <kalash@2degrees-investing.org>
2023-07-27 09:48:41 +0200

    Remove tests and merge `tiltIndicatorAfter_runner` into one .Rmd file

--- tests/testthat/test-prepare_istr_product.R
+++
@@ -1,6 +0,0 @@
-test_that("total number of rows for a comapny is either 1, 4, 12", {
-  out <- prepare_istr_product(istr_product, ep_companies, ecoinvent_activities, matches_mapper, ecoinvent_inputs) |>
-    group_by(companies_id, ep_product, input_name) |>
-    summarise(count = n())
-  expect_true(all(unique(out$count) %in% c(1, 4, 12)))
-})
