b0203c2c2c1407ea374eb98b7c99b2da32b4f026
Mauro Lepore <maurolepore@gmail.com>
2023-09-21 23:13:59 +0000

    New small_matches_mapper to run tests faster

--- tests/testthat/test-prepare_istr_company.R
+++ tests/testthat/test-prepare_istr_company.R
@@ -1,5 +1,5 @@
 test_that("total number of rows for a comapny is either 1 or 3", {
-  out <- prepare_istr_company(istr_company, istr_product, ep_companies, ecoinvent_activities, matches_mapper, ecoinvent_inputs) |>
+  out <- prepare_istr_company(istr_company, istr_product, ep_companies, ecoinvent_activities, small_matches_mapper, ecoinvent_inputs) |>
     group_by(companies_id, scenario, year) |>
     summarise(count = n())
   expect_true(all(unique(out$count) %in% c(1, 3)))
--- tests/testthat/test-prepare_matches_mapper.R
+++ tests/testthat/test-prepare_matches_mapper.R
@@ -1,9 +1,9 @@
 test_that("`multi_match` column has either `TRUE` or `FALSE` values", {
-  out <- prepare_matches_mapper(matches_mapper, ecoinvent_activities)
+  out <- prepare_matches_mapper(small_matches_mapper, ecoinvent_activities)
   expect_equal(unique(out$multi_match), c("TRUE", "FALSE"))
 })
 
 test_that("`completion` column has either `low`, `medium`, or `high` values", {
-  out <- prepare_matches_mapper(matches_mapper, ecoinvent_activities)
+  out <- prepare_matches_mapper(small_matches_mapper, ecoinvent_activities)
   expect_equal(unique(out$completion), c("low", "high", "medium"))
 })
