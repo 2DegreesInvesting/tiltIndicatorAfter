ecd3d87d234273449df69ec72dc45e1da75c21a6
Mauro Lepore <maurolepore@gmail.com>
2023-09-23 17:16:54 -0300

    Style (#44)

--- data-raw/ictr.R
+++ data-raw/ictr.R
@@ -6,7 +6,8 @@ devtools::load_all()
 # Source:
 
 ictr_product <- read_csv(here("data-raw", "sample_input_datasets", "ictr_product_level_sample.csv"),
-                         col_types = cols(input_isic_4digit = col_character()))
+  col_types = cols(input_isic_4digit = col_character())
+)
 ictr_company <- read_csv(here("data-raw", "sample_input_datasets", "ictr_company_level_sample.csv"))
 
 use_data(ictr_product, overwrite = TRUE)
