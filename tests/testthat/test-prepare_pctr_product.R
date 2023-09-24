02418329750760059219f53848857e1d56dcf6ba
Mauro Lepore <maurolepore@gmail.com>
2023-09-23 22:40:57 -0300

    `prepare_pctr_product()` errors with "Column `unit` doesn't exist" (#26)

--- tests/testthat/test-prepare_pctr_product.R
+++ tests/testthat/test-prepare_pctr_product.R
@@ -16,3 +16,25 @@ test_that("handles numeric `isic*`", {
     )
   )
 })
+
+test_that("doesn't throw error: 'Column unit doesn't exist' (#26)", {
+  emissions_profile_product <- tibble(
+    companies_id = "a",
+    grouped_by = "all",
+    risk_category = "high",
+    co2_footprint = 1,
+    tilt_sector = "Industry",
+    tilt_subsector = "Other",
+    isic_4digit = "2560"
+  )
+
+  expect_no_error(
+    prepare_pctr_product(
+      emissions_profile_product,
+      ep_companies |> head(1),
+      ecoinvent_activities |> head(1),
+      small_matches_mapper |> head(1),
+      isic_tilt_mapper |> head(1)
+    )
+  )
+})
