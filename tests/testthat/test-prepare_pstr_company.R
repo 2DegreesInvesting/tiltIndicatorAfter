23651148b336432d539a136f00fb2b7fcf2ca91d
Mauro Lepore <maurolepore@gmail.com>
2023-09-24 16:09:46 -0300

    * Accept the output of `sector_profile()` 'as is' (#50)

--- tests/testthat/test-prepare_pstr_company.R
+++ tests/testthat/test-prepare_pstr_company.R
@@ -1,6 +1,25 @@
 test_that("total number of rows for a comapny is either 1 or 3", {
-  out <- prepare_pstr_company(pstr_company, pstr_product, ep_companies, ecoinvent_activities, small_matches_mapper) |>
+  out <- prepare_pstr_company(
+    unnest_company(sector_profile_result()),
+    unnest_product(sector_profile_result()),
+    ep_companies,
+    ecoinvent_activities,
+    small_matches_mapper
+  ) |>
     group_by(companies_id, scenario, year) |>
     summarise(count = n())
+
   expect_true(all(unique(out$count) %in% c(1, 3)))
+})
+
+test_that("throws no error", {
+  expect_no_error(
+    prepare_pstr_company(
+      unnest_company(sector_profile_result()) |> head(1),
+      unnest_product(sector_profile_result()) |> head(1),
+      ep_companies |> head(1),
+      ecoinvent_activities |> head(1),
+      matches_mapper |> head(1)
+    )
+  )
 })
