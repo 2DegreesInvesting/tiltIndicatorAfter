1c1779279945bffb9700016cf29a99ea1cf03280
Mauro Lepore <maurolepore@gmail.com>
2023-09-24 00:22:47 +0000

    Import stringr::str_pad

--- R/pad_column.R
+++ R/pad_column.R
@@ -4,7 +4,7 @@ pad_column <- function(data, pattern, width, pad) {
 
   x <- get_column(data, pattern)
   if (!is.character(x)) {
-    data[extract_name(data, pattern)] <- stringr::str_pad(x, width = width, pad = pad)
+    data[extract_name(data, pattern)] <- str_pad(x, width = width, pad = pad)
   }
 
   data
