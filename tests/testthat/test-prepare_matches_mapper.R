ee58fd70584cc97573c8878db9344a4b92372547
Kalash Singhal <kalash@2degrees-investing.org>
2023-10-11 13:58:30 +0200

    test non-missing outputs

--- tests/testthat/test-prepare_matches_mapper.R
+++ tests/testthat/test-prepare_matches_mapper.R
@@ -7,3 +7,28 @@ test_that("`completion` column has either `low`, `medium`, or `high` values", {
   out <- prepare_matches_mapper(small_matches_mapper, ecoinvent_activities)
   expect_equal(unique(out$completion), c("low", "high", "medium"))
 })
+
+test_that("prepare_matches_mapper doesn't have any empty string", {
+  matches_mapper <- tibble(
+    ep_id = c("b", "a", "all"),
+    country = c("b", "a", "all"),
+    main_activity = c("b", "a", "all"),
+    clustered = c("b", "a", "all"),
+    activity_uuid_product_uuid = c("b", "any", NA_character_),
+    multi_match = c("b", "a", "all"),
+    completion = c("b", "a", "all"),
+    category = c("b", "a", "all")
+  )
+
+  ecoinvent_activities <- tibble(
+    activity_uuid_product_uuid = c("any", NA_character_),
+    activity_name = c(NA_character_, "c"),
+    geography = c("d", NA_character_),
+    reference_product_name = c("d", NA_character_),
+    unit = c("d", "c")
+  )
+
+  out <- prepare_matches_mapper(matches_mapper, ecoinvent_activities)
+  final <- unlist(out)
+  expect_false(any(final[!is.na(final)] == ""))
+})
