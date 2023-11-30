08be94daf21d97577c01465cc3e908891ef4ace9
Mauro Lepore <maurolepore@gmail.com>
2023-11-30 10:48:21 +0000

    Test with upstream products

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -11,3 +11,13 @@ test_that("sanitize_co2() converts `*isic*` as character", {
   out <- sanitize_isic(read_csv("isic_4digit\n1"))
   expect_type(out$isic_4digit, "character")
 })
+
+test_that("sanitize_co2() works with both products and upstream products", {
+  local_options(readr.show_col_types = FALSE)
+
+  out <- sanitize_isic(tibble(isic_4digit = 1))
+  expect_equal(out$isic_4digit, c("1"))
+
+  out <- sanitize_isic(tibble(input_isic_4digit = 1))
+  expect_equal(out$input_isic_4digit, c("1"))
+})
