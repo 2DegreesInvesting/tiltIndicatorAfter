41bb309da0940ef69591c30e1cc4bfd01dcc9f58
Mauro Lepore <maurolepore@gmail.com>
2023-12-01 08:56:01 +0000

    TDD fail

--- tests/testthat/test-profile_sector.R
+++ tests/testthat/test-profile_sector.R
@@ -7,6 +7,15 @@ test_that("the new API is equivalent to the old API", {
   ecoinvent_activities <- ecoinvent_activities |> head(3)
   ecoinvent_europages <- small_matches_mapper |> head(3)
 
+  # New API
+  out <- profile_sector(
+    companies,
+    scenarios,
+    europages_companies,
+    ecoinvent_activities,
+    ecoinvent_europages
+  )
+
   # Old API
   output <- sector_profile(companies, scenarios)
   product <- unnest_product(output)
@@ -27,15 +36,6 @@ test_that("the new API is equivalent to the old API", {
     ecoinvent_europages
   )
 
-  # New API
-  out <- profile_sector(
-    companies,
-    scenarios,
-    europages_companies,
-    ecoinvent_activities,
-    ecoinvent_europages
-  )
-
   expect_equal(
     out |> unnest_product() |> arrange(companies_id),
     out_product |> arrange(companies_id)
@@ -44,4 +44,26 @@ test_that("the new API is equivalent to the old API", {
     out |> unnest_company() |> arrange(companies_id),
     out_company |> arrange(companies_id)
   )
+})
+
+test_that("the output at product level has columns matching isic and sector", {
+  local_options(readr.show_col_types = FALSE)
+
+  companies <- read_csv(toy_sector_profile_companies())
+  scenarios <- read_csv(toy_sector_profile_any_scenarios())
+  europages_companies <- ep_companies |> head(3)
+  ecoinvent_activities <- ecoinvent_activities |> head(3)
+  ecoinvent_europages <- small_matches_mapper |> head(3)
+
+  out <- profile_sector(
+    companies,
+    scenarios,
+    europages_companies,
+    ecoinvent_activities,
+    ecoinvent_europages
+  )
+
+  product <- unnest_product(out)
+  expect_true(any(matches_name(product, "isic")))
+  expect_true(any(matches_name(product, "sector")))
 })
