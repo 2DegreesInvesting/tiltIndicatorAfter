21afb560183fe70a345e8a9cdb65999db2d2f263
Mauro Lepore <maurolepore@gmail.com>
2023-12-04 07:20:14 +0000

    DRY ictr_company

--- tests/testthat/test-prepare_ictr_company.R
+++ tests/testthat/test-prepare_ictr_company.R
@@ -1,84 +1,41 @@
-test_that("total number of rows for a comapny is either 1 or 3", {
+# TODO: Move to test-profile_emissions_upstream.R
+test_that("the total number of rows for a comapny is either 1 or 3", {
   local_options(readr.show_col_types = FALSE)
 
   companies <- read_csv(toy_emissions_profile_any_companies())
-  co2 <- read_csv(toy_emissions_profile_upstream_products()) |>
-    # FIXME: Handle this in tiltIndicator
-    add_rowid()
-  output <- emissions_profile_upstream(companies, co2)
+  co2 <- read_csv(toy_emissions_profile_upstream_products())
 
-
-  company <- unnest_company(output)
-  product <- unnest_product(output) |>
-    # FIXME: Handle this inside the new interface
-    left_join(select(co2, matches(extra_cols_pattern())), by = extra_rowid())
-
-  out <- prepare_ictr_company(
-    company,
-    product,
-    ep_companies,
+  out <- profile_emissions_upstream(
+    companies,
+    co2,
+    europages_companies = ep_companies,
     ecoinvent_activities,
-    small_matches_mapper,
-    ecoinvent_inputs,
-    isic_tilt_mapper
+    ecoinvent_inputs = ecoinvent_inputs,
+    ecoinvent_europages = small_matches_mapper,
+    isic_tilt =isic_tilt_mapper
   ) |>
+    unnest_company() |>
     group_by(companies_id, benchmark) |>
     summarise(count = n())
+
   expect_true(all(unique(out$count) %in% c(1, 3)))
 })
 
-test_that("handles numeric `isic*`", {
+test_that("handles numeric `isic*` in `co2`", {
   local_options(readr.show_col_types = FALSE)
 
   companies <- read_csv(toy_emissions_profile_any_companies())
-  co2 <- read_csv(toy_emissions_profile_upstream_products()) |>
-    # FIXME: Handle this in tiltIndicator
-    add_rowid()
-  output <- emissions_profile_upstream(companies, co2)
-
-
-  company <- unnest_company(output)
-  product <- unnest_product(output) |>
-    # FIXME: Handle this inside the new interface
-    left_join(select(co2, matches(extra_cols_pattern())), by = extra_rowid())
+  co2 <- read_csv(toy_emissions_profile_upstream_products())
 
   expect_no_error(
-    prepare_ictr_company(
-      company |> head(3),
-      product |> head(3) |> modify_col("isic", as.numeric),
-      ep_companies |> head(3),
-      ecoinvent_activities |> head(3),
-      small_matches_mapper |> head(3),
-      ecoinvent_inputs |> head(3),
-      isic_tilt_mapper |> head(3)
-    )
-  )
-})
-
-test_that("takes the outpus of tiltIndicator", {
-  local_options(readr.show_col_types = FALSE)
-
-  companies <- read_csv(toy_emissions_profile_any_companies())
-  co2 <- read_csv(toy_emissions_profile_upstream_products()) |>
-    # FIXME: Handle this in tiltIndicator
-    add_rowid()
-  output <- emissions_profile_upstream(companies, co2)
-
-
-  company <- unnest_company(output)
-  product <- unnest_product(output) |>
-    # FIXME: Handle this inside the new interface
-    left_join(select(co2, matches(extra_cols_pattern())), by = extra_rowid())
-
-  expect_no_error(
-    prepare_ictr_company(
-      company |> head(3),
-      product |> head(3),
-      ep_companies |> head(3),
-      ecoinvent_activities |> head(3),
-      small_matches_mapper |> head(3),
-      ecoinvent_inputs |> head(3),
-      isic_tilt_mapper |> head(3)
+    profile_emissions_upstream(
+      companies,
+      co2 |> modify_col("isic", as.numeric),
+      europages_companies = ep_companies,
+      ecoinvent_activities,
+      ecoinvent_inputs = ecoinvent_inputs,
+      ecoinvent_europages = small_matches_mapper,
+      isic_tilt = isic_tilt_mapper
     )
   )
 })
