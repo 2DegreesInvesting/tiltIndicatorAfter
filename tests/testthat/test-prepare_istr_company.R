f18a7c26cafc4392c3a42bf5b62ed0a6afb4e294
Mauro Lepore <maurolepore@gmail.com>
2023-12-04 08:38:10 +0000

    DRY profile_sector_upstream()

--- tests/testthat/test-prepare_istr_company.R
+++ tests/testthat/test-prepare_istr_company.R
@@ -1,34 +1,20 @@
 test_that("total number of rows for a comapny is either 1 or 3", {
-  company <- unnest_company(toy_sector_profile_upstream_output())
-  product <- unnest_product(toy_sector_profile_upstream_output())
+  companies <- read_csv(toy_sector_profile_upstream_companies())
+  scenarios <- read_csv(toy_sector_profile_any_scenarios())
+  inputs <- read_csv(toy_sector_profile_upstream_products())
 
-  out <- prepare_istr_company(
-    company,
-    product,
-    ep_companies,
-    ecoinvent_activities,
-    small_matches_mapper,
-    ecoinvent_inputs,
-    isic_tilt_mapper
+  out <- profile_sector_upstream(
+    companies,
+    scenarios,
+    inputs,
+    europages_companies = ep_companies,
+    ecoinvent_activities = ecoinvent_activities,
+    ecoinvent_inputs = ecoinvent_inputs,
+    ecoinvent_europages = small_matches_mapper,
+    isic_tilt = isic_tilt_mapper
   ) |>
+    unnest_company() |>
     group_by(companies_id, scenario, year) |>
     summarise(count = n())
   expect_true(all(unique(out$count) %in% c(1, 3)))
-})
-
-test_that("handles tiltIndicator output", {
-  company <- unnest_company(toy_sector_profile_upstream_output())
-  product <- unnest_product(toy_sector_profile_upstream_output())
-
-  expect_no_error(
-    prepare_istr_company(
-      company |> head(3),
-      product |> head(3),
-      ep_companies |> head(3),
-      ecoinvent_activities |> head(3),
-      matches_mapper |> head(3),
-      ecoinvent_inputs |> head(3),
-      isic_tilt_mapper |> head(3)
-    )
-  )
 })
