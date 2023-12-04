69932b4cb8e7737eb85b916021468fd0790e30dd
Mauro Lepore <maurolepore@gmail.com>
2023-12-04 13:54:38 +0000

    Remove redundant test

--- tests/testthat/test-prepare_istr_company.R
+++ tests/testthat/test-prepare_istr_company.R
@@ -20,20 +20,3 @@ test_that("total number of rows for a comapny is either 1 or 3", {
     summarise(count = n())
   expect_true(all(unique(out$count) %in% c(1, 3)))
 })
-
-test_that("handles tiltIndicator output", {
-  company <- unnest_company(toy_sector_profile_upstream_output())
-  product <- unnest_product(toy_sector_profile_upstream_output())
-
-  expect_no_error(
-    prepare_istr_company(
-      company |> head(3),
-      product |> head(3),
-      ep_companies |> head(3),
-      ecoinvent_activities |> head(3),
-      matches_mapper |> head(3),
-      ecoinvent_inputs |> head(3),
-      isic_tilt_mapper |> head(3)
-    )
-  )
-})
