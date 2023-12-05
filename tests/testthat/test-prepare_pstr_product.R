04585bd4118d6e60a7cecb0726dec10ad88e3993
Mauro Lepore <maurolepore@gmail.com>
2023-12-05 20:52:43 +0000

    Rewrite tests using the new API

--- tests/testthat/test-prepare_pstr_product.R
+++ tests/testthat/test-prepare_pstr_product.R
@@ -16,43 +16,56 @@ test_that("total number of rows for a comapny is either 1, 2 or 4", {
 })
 
 test_that("'empty' tiltIndicator results yield at most 1 NA in *risk_category", {
-  skip("FIXME: Rewrite using the new API")
+  local_options(readr.show_col_types = FALSE)
 
-  product <- unnest_product(toy_sector_profile_output())
-  product_empty <- product[1, ]
-  product_empty[1, "companies_id"] <- "a"
-  product_empty[1, "risk_category"] <- NA_character_
+  companies <- read_csv(toy_sector_profile_companies())
+  scenarios_that_yields_na <- read_csv(toy_sector_profile_any_scenarios()) |>
+    head(1)
 
-  result <- prepare_pstr_product(
-    product_empty,
-    ep_companies,
-    ecoinvent_activities,
-    small_matches_mapper,
-    isic_tilt_mapper
-  )
-  out <- result |>
-    filter(is.na(get_column(result, "risk_category"))) |>
-    group_by(companies_id) |>
-    summarise(count = n())
+  result <- profile_sector(
+    companies,
+    scenarios_that_yields_na,
+    europages_companies = ep_companies,
+    ecoinvent_activities = ecoinvent_activities,
+    ecoinvent_europages = small_matches_mapper,
+    isic_tilt = isic_tilt_mapper
+  ) |>
+    unnest_product()
 
-  expect_lte(unique(out$count), 1)
+  na <- filter(result, is.na(get_column(result, "risk_category")))
+  # Ensure we have many missing values
+  stopifnot(nrow(na) > 5)
+  # Ensure we have many companies
+  stopifnot(length(unique(na$companies_id)) > 5)
+
+  number_of_na_per_company <- count(na, companies_id)$n
+  expect_equal(unique(number_of_na_per_company), 1)
 })
 
 test_that("yield NA in `*tilt_sector` and `*tilt_subsector` for no risk category", {
-  skip("FIXME: Rewrite using the new API")
+  local_options(readr.show_col_types = FALSE)
 
-  product <- unnest_product(toy_sector_profile_output())
-  product_empty <- product[1, ]
-  product_empty[1, "risk_category"] <- NA_character_
+  companies <- read_csv(toy_sector_profile_companies())
+  scenarios_that_yields_na <- read_csv(toy_sector_profile_any_scenarios()) |>
+    head(1)
 
-  result <- prepare_pstr_product(
-    product_empty,
-    ep_companies,
-    ecoinvent_activities,
-    small_matches_mapper,
-    isic_tilt_mapper
-  )
+  result <- profile_sector(
+    companies,
+    scenarios_that_yields_na,
+    europages_companies = ep_companies,
+    ecoinvent_activities = ecoinvent_activities,
+    ecoinvent_europages = small_matches_mapper,
+    isic_tilt = isic_tilt_mapper
+  ) |>
+    unnest_product()
 
-  expect_true(all(is.na(select(result, tilt_sector, tilt_subsector))))
+  na <- filter(result, is.na(get_column(result, "risk_category")))
+  # Ensure we have many missing values
+  stopifnot(nrow(na) > 5)
+  # Ensure we have many companies
+  stopifnot(length(unique(na$companies_id)) > 5)
+
+  these_cols_are_full_of_na <- all(is.na(select(na, tilt_sector, tilt_subsector)))
+  expect_true(these_cols_are_full_of_na)
 })
 
