9ca0ab8c9c063ff7916de9f4b93545df721063de
Mauro Lepore <maurolepore@gmail.com>
2023-12-22 14:05:40 +0000

    Fix version

--- data/deprecated.R
+++ data/deprecated.R
@@ -1,6 +1,6 @@
 
 delayedAssign("isic_tilt_mapper", value = {
-  msg <- "`isic_tilt_mapper` was deprecated in 0.0.0.9018. Please use `isic`."
+  msg <- "`isic_tilt_mapper` was deprecated in 0.0.0.9017. Please use `isic`."
 
   on_rcmd <- nzchar(Sys.getenv("R_CMD"))
   if (!on_rcmd) .Deprecated(msg = msg)
