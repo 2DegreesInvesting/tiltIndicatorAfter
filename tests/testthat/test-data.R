b956a3285e47ff63876833f450a4c3e0b413f966
Mauro Lepore <maurolepore@gmail.com>
2024-01-14 21:19:54 +0000

    Refactor

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -21,7 +21,3 @@ test_that("`pstr_product` hasn't changed", {
 test_that("`istr_product` hasn't changed", {
   expect_snapshot(format_minimal_snapshot(tiltIndicatorAfter::istr_product))
 })
-
-test_that("`isic_name` hasn't changed", {
-  expect_snapshot(format_minimal_snapshot(read_csv(toy_isic_name())))
-})
