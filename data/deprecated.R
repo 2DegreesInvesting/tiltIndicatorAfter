c03b547e2a05b7762fe1dd30fe1b254f9d86db84
Mauro Lepore <maurolepore@gmail.com>
2024-01-14 21:34:08 +0000

    Refactor

--- data/deprecated.R
+++ data/deprecated.R
@@ -1,8 +1,8 @@
 delayedAssign("isic_tilt_mapper", value = {
-  msg <- "`isic_tilt_mapper` was deprecated in 0.0.0.9017. Please use `read_csv(tiltToyData::toy_isic_name())`."
+  msg <- "`isic_tilt_mapper` was deprecated in 0.0.0.9017. Please use `readr::read_csv(tiltToyData::toy_isic_name())`."
 
   on_rcmd <- nzchar(Sys.getenv("R_CMD"))
   if (!on_rcmd) .Deprecated(msg = msg)
 
-  read_csv(toy_isic_name())
+  readr::read_csv(tiltToyData::toy_isic_name(), show_col_types = FALSE)
 })
