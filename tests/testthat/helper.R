7299be0f8adb018e63b13ee1d2bcd28be4f3a145
Mauro Lepore <maurolepore@gmail.com>
2024-01-14 13:49:54 +0000

    Quiet lifecycle_verbosity

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,1 +1,11 @@
 small_matches_mapper <- head(tiltIndicatorAfter::matches_mapper, 100)
+
+toy_emissions_profile_products <- function() {
+  withr::local_options(lifecycle_verbosity = "quiet")
+  tiltToyData::toy_emissions_profile_products()
+}
+
+toy_emissions_profile_upstream_products <- function() {
+  withr::local_options(lifecycle_verbosity = "quiet")
+  tiltToyData::toy_emissions_profile_upstream_products()
+}
