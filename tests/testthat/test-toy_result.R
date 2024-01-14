71882619869b918645b181a7c75c84b68185995f
Mauro Lepore <maurolepore@gmail.com>
2024-01-14 15:05:17 +0000

    Prune

--- tests/testthat/test-toy_result.R
+++ tests/testthat/test-toy_result.R
@@ -1,34 +1,10 @@
 test_that("toy_emissions_profile_output hasn't changed", {
-  # FIXME: Prune #121
-  skip_unless_toy_data_is_newer_than(toy_data_version())
-  toy_emissions_profile_output <- function() {
-    toy_output(
-      emissions_profile,
-      list(
-        toy_emissions_profile_any_companies(),
-        tiltToyData::toy_emissions_profile_products_ecoinvent()
-      )
-    )
-  }
-
   out <- toy_emissions_profile_output()
   expect_snapshot(format_minimal_snapshot(unnest_product(out)))
   expect_snapshot(format_minimal_snapshot(unnest_company(out)))
 })
 
 test_that("toy_emissions_profile_upstream_output hasn't changed", {
-  # FIXME: Prune #121
-  skip_unless_toy_data_is_newer_than(toy_data_version())
-  toy_emissions_profile_upstream_output <- function() {
-    toy_output(
-      emissions_profile_upstream,
-      list(
-        toy_emissions_profile_any_companies(),
-        tiltToyData::toy_emissions_profile_upstream_products_ecoinvent()
-      )
-    )
-  }
-
   out <- toy_emissions_profile_upstream_output()
   expect_snapshot(format_minimal_snapshot(unnest_product(out)))
   expect_snapshot(format_minimal_snapshot(unnest_company(out)))
