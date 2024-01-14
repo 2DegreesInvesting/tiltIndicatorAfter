b4978dda210a7c2d72640e604101771a94d5d5b6
Mauro Lepore <maurolepore@gmail.com>
2024-01-14 21:01:23 +0000

    Refactor

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -22,9 +22,6 @@ test_that("`istr_product` hasn't changed", {
   expect_snapshot(format_minimal_snapshot(tiltIndicatorAfter::istr_product))
 })
 
-test_that("`ecoinvent_inputs` hasn't changed", {
-  expect_snapshot(format_minimal_snapshot(read_csv(toy_ecoinvent_inputs())))
-})
 
 test_that("`matches_mapper` hasn't changed", {
   expect_snapshot(format_minimal_snapshot(tiltIndicatorAfter::matches_mapper))
