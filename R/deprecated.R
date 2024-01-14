373bda4d302e13ee33bb7e19a18bb0ee7ffb7bf8
Mauro Lepore <maurolepore@gmail.com>
2024-01-14 21:49:09 +0000

    Deprecate

--- R/deprecated.R
+++ R/deprecated.R
@@ -4,3 +4,38 @@
 #'
 #' @keywords internal
 "isic_tilt_mapper"
+
+#' Deprecated in favor of an equivalent dataset in tiltToyData
+#'
+#' `r lifecycle::badge("deprecated")`
+#'
+#' @keywords internal
+"ep_companies"
+
+#' Deprecated in favor of an equivalent dataset in tiltToyData
+#'
+#' `r lifecycle::badge("deprecated")`
+#'
+#' @keywords internal
+"ecoinvent_activities"
+
+#' Deprecated in favor of an equivalent dataset in tiltToyData
+#'
+#' `r lifecycle::badge("deprecated")`
+#'
+#' @keywords internal
+"ecoinvent_inputs"
+
+#' Deprecated in favor of an equivalent dataset in tiltToyData
+#'
+#' `r lifecycle::badge("deprecated")`
+#'
+#' @keywords internal
+"matches_mapper"
+
+#' Deprecated in favor of an equivalent dataset in tiltToyData
+#'
+#' `r lifecycle::badge("deprecated")`
+#'
+#' @keywords internal
+"isic_name"
