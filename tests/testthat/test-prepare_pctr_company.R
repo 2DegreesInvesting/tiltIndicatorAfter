6ab92952e54469bb6ef38f05b4b8978f10d7d0d4
Mauro Lepore <maurolepore@gmail.com>
2024-01-14 21:10:50 +0000

    Refactor

--- tests/testthat/test-prepare_pctr_company.R
+++ tests/testthat/test-prepare_pctr_company.R
@@ -11,7 +11,7 @@ test_that("total number of rows for a comapny is either 1 or 3", {
     co2,
     europages_companies = read_csv(toy_europages_companies()),
     ecoinvent_activities = read_csv(toy_ecoinvent_activities()),
-    ecoinvent_europage = small_matches_mapper,
+    ecoinvent_europages = read_csv(toy_ecoinvent_europages()),
     isic = isic_name
   ) |>
     unnest_company() |>
@@ -32,7 +32,7 @@ test_that("handles numeric `isic*` in `co2`", {
       co2 |> modify_col("isic", unquote) |> modify_col("isic", as.numeric),
       europages_companies = read_csv(toy_europages_companies()),
       ecoinvent_activities = read_csv(toy_ecoinvent_activities()),
-      ecoinvent_europage = small_matches_mapper,
+      ecoinvent_europages = read_csv(toy_ecoinvent_europages()),
       isic = isic_name
     )
   )
