7b185cb44422a8b601da24101e1cd2723a5691a8
Mauro Lepore <maurolepore@gmail.com>
2024-01-14 21:25:22 +0000

    Refactor

--- tests/testthat/test-prepare_pstr_product.R
+++ tests/testthat/test-prepare_pstr_product.R
@@ -8,7 +8,7 @@ test_that("total number of rows for a comapny is either 1, 2 or 4", {
     read_csv(toy_europages_companies()),
     read_csv(toy_ecoinvent_activities()),
     read_csv(toy_ecoinvent_europages()),
-    isic
+    read_csv(toy_isic_name())
   ) |>
     group_by(companies_id, ep_product, activity_uuid_product_uuid) |>
     summarise(count = n())
