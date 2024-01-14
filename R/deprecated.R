1c6255bdd312c3d49bd9997211b65d7db8991d09
Mauro Lepore <maurolepore@gmail.com>
2024-01-14 22:10:52 +0000

    Add-examples

--- R/deprecated.R
+++ R/deprecated.R
@@ -10,6 +10,11 @@
 #' `r lifecycle::badge("deprecated")`
 #'
 #' @keywords internal
+#' @examples
+#' ep_companies
+#' # ->
+#' readr::read_csv(tiltToyData::toy_europages_companies())
+#'
 "ep_companies"
 
 #' Deprecated in favor of an equivalent dataset in tiltToyData
@@ -17,6 +22,10 @@
 #' `r lifecycle::badge("deprecated")`
 #'
 #' @keywords internal
+#' @examples
+#' ecoinvent_activities
+#' # ->
+#' readr::read_csv(tiltToyData::toy_ecoinvent_activities())
 "ecoinvent_activities"
 
 #' Deprecated in favor of an equivalent dataset in tiltToyData
@@ -24,6 +33,10 @@
 #' `r lifecycle::badge("deprecated")`
 #'
 #' @keywords internal
+#' @examples
+#' ecoinvent_inputs
+#' # ->
+#' readr::read_csv(tiltToyData::toy_ecoinvent_inputs())
 "ecoinvent_inputs"
 
 #' Deprecated in favor of an equivalent dataset in tiltToyData
@@ -31,6 +44,10 @@
 #' `r lifecycle::badge("deprecated")`
 #'
 #' @keywords internal
+#' @examples
+#' matches_mapper
+#' # ->
+#' readr::read_csv(tiltToyData::toy_ecoinvent_europages())
 "matches_mapper"
 
 #' Deprecated in favor of an equivalent dataset in tiltToyData
@@ -38,4 +55,8 @@
 #' `r lifecycle::badge("deprecated")`
 #'
 #' @keywords internal
+#' @examples
+#' isic_name
+#' # ->
+#' readr::read_csv(tiltToyData::toy_isic_name())
 "isic_name"
