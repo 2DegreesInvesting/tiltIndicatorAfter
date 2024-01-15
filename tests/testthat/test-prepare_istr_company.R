807a4b0623d17906466a585807c146d98a7c632d
Mauro Lepore <maurolepore@gmail.com>
2024-01-15 20:42:30 +0000

    wip

--- tests/testthat/test-prepare_istr_company.R
+++ tests/testthat/test-prepare_istr_company.R
@@ -5,15 +5,21 @@ test_that("total number of rows for a comapny is either 1 or 3", {
   scenarios <- read_csv(toy_sector_profile_any_scenarios())
   inputs <- read_csv(toy_sector_profile_upstream_products())
 
+  europages_companies <- read_csv(toy_europages_companies())
+  ecoinvent_activities <- read_csv(toy_ecoinvent_activities())
+  ecoinvent_inputs <- read_csv(toy_ecoinvent_inputs())
+  ecoinvent_europages <- read_csv(toy_ecoinvent_europages())
+  isic <- read_csv(toy_isic_name())
+
   out <- profile_sector_upstream(
     companies,
     scenarios,
     inputs,
-    europages_companies = read_csv(toy_europages_companies()),
-    ecoinvent_activities = read_csv(toy_ecoinvent_activities()),
-    ecoinvent_inputs = read_csv(toy_ecoinvent_inputs()),
-    ecoinvent_europages = read_csv(toy_ecoinvent_europages()),
-    isic = read_csv(toy_isic_name())
+    europages_companies = europages_companies,
+    ecoinvent_activities = ecoinvent_activities,
+    ecoinvent_inputs = ecoinvent_inputs,
+    ecoinvent_europages = ecoinvent_europages,
+    isic = isic_name
   ) |>
     unnest_company() |>
     group_by(companies_id, scenario, year) |>
