10fd326d0359f4527eb6f9156c86fd53568f67d2
Mauro Lepore <maurolepore@gmail.com>
2024-01-15 19:29:30 +0000

    Skip on CI

--- tests/testthat/test-deprecated.R
+++ tests/testthat/test-deprecated.R
@@ -1,4 +1,6 @@
 test_that("`ep_companies` is deprecated", {
+  skip_on_ci()
+
   expect_snapshot(expect_equal(
     readr::read_csv(tiltToyData::toy_europages_companies()),
     ep_companies
@@ -6,6 +8,8 @@ test_that("`ep_companies` is deprecated", {
 })
 
 test_that("`ecoinvent_activities` is deprecated", {
+  skip_on_ci()
+
   expect_snapshot(expect_equal(
     readr::read_csv(tiltToyData::toy_ecoinvent_activities()),
     ecoinvent_activities
@@ -13,6 +17,8 @@ test_that("`ecoinvent_activities` is deprecated", {
 })
 
 test_that("`ecoinvent_inputs` is deprecated", {
+  skip_on_ci()
+
   expect_snapshot(expect_equal(
     readr::read_csv(tiltToyData::toy_ecoinvent_inputs()),
     ecoinvent_inputs
@@ -20,6 +26,8 @@ test_that("`ecoinvent_inputs` is deprecated", {
 })
 
 test_that("`matches_mapper` is deprecated", {
+  skip_on_ci()
+
   expect_snapshot(expect_equal(
     readr::read_csv(tiltToyData::toy_ecoinvent_europages()),
     matches_mapper
@@ -27,6 +35,8 @@ test_that("`matches_mapper` is deprecated", {
 })
 
 test_that("`isic_name` is deprecated", {
+  skip_on_ci()
+
   expect_snapshot(expect_equal(
     readr::read_csv(tiltToyData::toy_isic_name()),
     isic_name
