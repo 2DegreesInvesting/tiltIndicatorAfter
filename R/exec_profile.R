8ee127681b1cc13ef4af9b90307e403cca75bfda
Kalash Singhal <kalash@2degrees-investing.org>
2024-02-16 07:47:32 +0100

    resolve error

--- R/exec_profile.R
+++ R/exec_profile.R
@@ -4,7 +4,6 @@ exec_profile <- function(.fn, indicator, indicator_after) {
     product_raw <- unnest_product(tilt_indicator_output) |>
       extend_with_columns_from_arguments_of_tilt_indicator(indicator, .fn)
 
-    set.seed(111)
     co2_range <- create_co2_range(product_raw)
     product <- add_co2_upper_lower(product_raw, co2_range)
 
@@ -69,17 +68,4 @@ add_rowid <- function(data) {
 
 extra_rowid <- function() {
   "extra_rowid"
-}
-
-create_co2_range <- function(data) {
-  data |>
-    summarize_range(grep("co2_footprint", names(data)),
-                    .by = c("grouped_by", "risk_category")) |>
-    jitter_range() |>
-    select(-c("min", "max")) |>
-    rename(co2e_lower = "min_jitter", co2e_upper = "max_jitter")
-}
-
-add_co2_upper_lower <- function(data, co2_range) {
-  left_join(data, co2_range, by = join_by("grouped_by", "risk_category"))
 }
