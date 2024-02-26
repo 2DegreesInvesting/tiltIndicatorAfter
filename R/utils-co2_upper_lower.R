5459068524e5f4183a306085f5c22d15ec94b16d
Mauro Lepore <maurolepore@gmail.com>
2024-02-26 21:40:21 +0000

    Fix the myterious failing check?

--- R/utils-co2_upper_lower.R
+++ R/utils-co2_upper_lower.R
@@ -17,7 +17,16 @@ stop_if_percent_noise_more_than_100 <- function(data) {
     mutate(max_percent_noise = percent_noise(.data$max, .data$max_jitter),
            min_percent_noise = percent_noise(.data$min, .data$min_jitter))
 
-  if (any(check_noise$max_percent_noise > 100) | any(check_noise$min_percent_noise > 100)) {
+  bad_noise <- any(check_noise$max_percent_noise > 100) | any(check_noise$min_percent_noise > 100)
+  if (is.na(bad_noise)) {
+    rlang::warn(c(
+      "The noise data is `NA`",
+      i = "Are you using the correct data?"
+    ))
+    return(invisible(data))
+  }
+
+  if (bad_noise) {
     abort(glue("`jitter_range` should not add more than 100% of noise to `max` and `min` columns.
                Please readjust the value of `amount` parameter in `jitter_range` function"))
   }
