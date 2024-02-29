f94778b5a912c53405959edc501b6f546783ce3c
Mauro Lepore <maurolepore@gmail.com>
2024-02-29 12:28:09 +0000

    wip

--- tests/testthat/test-profile_emissions_upstream.R
+++ tests/testthat/test-profile_emissions_upstream.R
@@ -280,21 +280,24 @@ test_that("informs the mean noise percent", {
 
 test_that("allows yielding the licensed `min` and `max` columns", {
   companies <- read_csv(toy_emissions_profile_any_companies())
-  co2 <- read_csv(toy_emissions_profile_products_ecoinvent())
+  co2 <- read_csv(toy_emissions_profile_upstream_products_ecoinvent())
   europages_companies <- read_csv(toy_europages_companies())
   ecoinvent_activities <- read_csv(toy_ecoinvent_activities())
+  ecoinvent_inputs <- read_csv(toy_ecoinvent_inputs())
   ecoinvent_europages <- read_csv(toy_ecoinvent_europages())
   isic_name <- read_csv(toy_isic_name())
 
+
   local_seed(111)
   local_options(tiltIndicatorAfter.co2_keep_licensed_min_max = TRUE)
-  out <- profile_emissions(
+  out <- profile_emissions_upstream(
     companies,
     co2,
-    europages_companies,
-    ecoinvent_activities,
-    ecoinvent_europages,
-    isic_name
+    europages_companies = europages_companies,
+    ecoinvent_activities = ecoinvent_activities,
+    ecoinvent_inputs = ecoinvent_inputs,
+    ecoinvent_europages = ecoinvent_europages,
+    isic = isic_name
   )
 
   expect_true(hasName(unnest_product(out), "min"))
@@ -302,10 +305,6 @@ test_that("allows yielding the licensed `min` and `max` columns", {
   expect_true(hasName(unnest_company(out), "min"))
   expect_true(hasName(unnest_company(out), "max"))
 })
-
-
-
-
 
 test_that("outputs `profile_ranking_avg` at company level", {
   companies <- read_csv(toy_emissions_profile_any_companies())
