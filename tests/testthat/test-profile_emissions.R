74ad21e3754e048aeb391a395bbaeee0473850bb
Mauro Lepore <maurolepore@gmail.com>
2024-03-06 22:37:45 +0000

    tdd-fail

--- tests/testthat/test-profile_emissions.R
+++ tests/testthat/test-profile_emissions.R
@@ -398,6 +398,27 @@ test_that("allows yielding the licensed `co2_footprint` column at product level"
   expect_true(hasName(unnest_product(out), "co2_footprint"))
 })
 
+test_that("can optionally output `co2_avg` at company level", {
+  companies <- read_csv(toy_emissions_profile_any_companies())
+  co2 <- read_csv(toy_emissions_profile_products_ecoinvent())
+  europages_companies <- read_csv(toy_europages_companies())
+  ecoinvent_activities <- read_csv(toy_ecoinvent_activities())
+  ecoinvent_europages <- read_csv(toy_ecoinvent_europages())
+  isic_name <- read_csv(toy_isic_name())
+
+  local_options(tiltIndicatorAfter.co2_keep_licensed_footprint = TRUE)
+  out <- profile_emissions(
+    companies,
+    co2,
+    europages_companies,
+    ecoinvent_activities,
+    ecoinvent_europages,
+    isic_name
+  )
+
+  expect_true(hasName(unnest_product(out), "co2_avg"))
+})
+
 test_that("outputs `profile_ranking_avg` at company level", {
   companies <- read_csv(toy_emissions_profile_any_companies())
   co2 <- read_csv(toy_emissions_profile_products_ecoinvent())
