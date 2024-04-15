cd52a2b6af9477e9997f655f2c00bffffb9b6cc6
Mauro Lepore <maurolepore@gmail.com>
2024-04-15 22:29:45 +0000

    wip

--- R/group_benchmarks.R
+++ R/group_benchmarks.R
@@ -48,7 +48,7 @@ group_benchmark_impl <- function(x, all) {
   out
 }
 
-# FIXME: replace with summarize_benchmark()
+# FIXME: replace with summarize_benchmark_range()
 # FIXME: Move to tiltIndicator?
 summarize_range_by <- function(data, col, .by) {
   # FIXME? Handle `col` rather than "col"?
