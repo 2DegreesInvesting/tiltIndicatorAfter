e24af5bafa1210117973db596d749a40b611c8cf
Mauro Lepore <maurolepore@gmail.com>
2024-04-15 23:42:18 +0000

    Refactor

--- R/group_benchmarks.R
+++ R/group_benchmarks.R
@@ -1,7 +1,8 @@
 # TODO: Maybe move to tiltIndicator? Seems like business logic
 group_benchmark <- function(x, all) {
   out <- lapply(x, group_benchmark_impl, all = all)
-  setNames(out, x)
+  names(out) <- x
+  out
 }
 
 group_benchmark_impl <- function(x, all) {
