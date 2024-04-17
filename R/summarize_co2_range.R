79b9eb31f9393f4b55bba97e0d5104e46727086c
Mauro Lepore <maurolepore@gmail.com>
2024-04-17 15:30:36 +0000

    document

--- R/summarize_co2_range.R
+++ R/summarize_co2_range.R
@@ -1,3 +1,30 @@
+#' Summarize CO2 footprint
+#'
+#' @param data The output of `profile_emissions*()` at product level.
+#' @keywords internal
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @examples
+#' # styler: off
+#' data <- tibble::tribble(
+#'   ~benchmark, ~emission_profile, ~co2_footprint, ~unit, ~tilt_sector, ~tilt_subsector, ~isic_4digit,
+#'        "all",             "low",             1L,  "m2",    "sector1",    "subsector1",     "'1234'",
+#'        "all",             "low",             2L,  "m2",    "sector1",    "subsector1",     "'1234'",
+#'        "all",            "high",             3L,  "m2",    "sector1",    "subsector1",     "'1234'",
+#'        "all",            "high",             4L,  "m2",    "sector1",    "subsector1",     "'1234'",
+#'       "unit",             "low",             1L,  "m2",    "sector1",    "subsector1",     "'1234'",
+#'       "unit",             "low",             2L,  "m2",    "sector1",    "subsector1",     "'1234'",
+#'       "unit",            "high",             3L,  "m2",    "sector1",    "subsector1",     "'1234'",
+#'       "unit",            "high",             4L,  "m2",    "sector1",    "subsector1",     "'1234'",
+#' )
+#' # styler: off
+#'
+#' data |>
+#'   summarize_co2_range() |>
+#'   jitter_co2_range(amount = 1) |>
+#'   polish_co2_range()
 summarize_co2_range <- function(data) {
   .benchmark <- "benchmark"
   .all <- c(.benchmark, "emission_profile")
@@ -10,6 +37,8 @@ summarize_co2_range <- function(data) {
   reduce(out, bind_rows)
 }
 
+#' @export
+#' @rdname summarize_co2_range
 jitter_co2_range <- function(data, amount = 2) {
   data |>
     group_by(benchmark) |>
@@ -18,6 +47,8 @@ jitter_co2_range <- function(data, amount = 2) {
     purrr::reduce(bind_rows)
 }
 
+#' @export
+#' @rdname summarize_co2_range
 polish_co2_range <- function(data, ...) {
   data |>
     rename(co2_lower = "min_jitter", co2_upper = "max_jitter") |>
