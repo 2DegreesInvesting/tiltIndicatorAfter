16a48f84e656a432478afa77b73d7478219e3e83
Mauro Lepore <maurolepore@gmail.com>
2024-04-17 13:41:35 +0000

    New map_summarize_range()

--- R/summarize_benchmark.R
+++ R/summarize_benchmark.R
@@ -1,28 +1,23 @@
+summarize_benchmark_range <- function(data, benchmark) {
+  data |>
+    summarize_benchmark_range_impl() |>
+    polish_benchmark_range(out, benchmark)
+}
+
+polish_benchmark_range <- function(data, benchmark) {
+  data[[benchmark]] |>
+    jitter_range() |>
+    select(-min, -max) |>
+    rename(co2_lower = min_jitter, co2_upper = max_jitter)
+}
+
 summarize_benchmark_range_impl <- function(data) {
   .benchmark <- "benchmark"
   .all <- c(.benchmark, "emission_profile")
+
+  .x <- split(data, data[[.benchmark]])
+  col <- extract_name(data, "co2_footprint")
   .by <- group_benchmark(unique(data[[.benchmark]]), .all)
 
-  summarize_values_range_in_groups_by(
-    data = data,
-    .values = extract_name(data, "co2_footprint"),
-    .groups = .benchmark,
-    .by = .by
-  )
-}
-
-summarize_values_range_in_groups_by <- function(data, .values, .groups, .by) {
-  .x <- split(data, data[[.groups]])
-  out <- vector("list", length = length(.x))
-  names(out) <- names(.x)
-
-  for (i in names(.x)) {
-    out[[i]] <- summarize_range(
-      data = .x[[i]],
-      col = .data[[.values]],
-      .by = all_of(.by[[i]])
-    )
-  }
-
-  out
+  map_summarize_range(.x, col = col, .by = .by)
 }
---
+++ R/summarize_range_vectorized.R
@@ -0,0 +1,17 @@
+# TODO: Move to tiltIndicator
+map_summarize_range <- function(.x, col, .by = NULL, na.rm = FALSE) {
+  .x <- .x
+  # TODO check that .x is a list
+  # TODO check that .by is a named list
+  # TODO check that .by is a named list
+  # TODO check the relationship between the names of .x and .by
+
+  out <- vector("list", length = length(.x))
+  names(out) <- names(.x)
+  for (i in names(.x)) {
+    out[[i]] <- summarize_range(
+      .x[[i]], col = .data[[col]], .by = all_of(.by[[i]]), na.rm = na.rm)
+  }
+
+  out
+}
