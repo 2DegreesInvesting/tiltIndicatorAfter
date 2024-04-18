b1e4b9a8506b5966eea6fdbe34383059f9c20abf
Mauro Lepore <maurolepore@gmail.com>
2024-04-18 19:56:39 +0000

    Update standalone

--- R/import-standalone-name-pattern.R
+++ R/import-standalone-name-pattern.R
@@ -1,13 +1,20 @@
 # Standalone file: do not edit by hand
-# Source: <https://github.com/2degreesInvesting/tiltIndicator/blob/main/R/standalone-name-pattern.R>
+# Source: <https://github.com/2DegreesInvesting/tiltIndicator/blob/main/R/standalone-name-pattern.R>
 # ----------------------------------------------------------------------
 #
+# ---
+# repo: 2DegreesInvesting/tiltIndicator
+# file: standalone-name-pattern.R
+# last-updated: 2024-04-18
+# license: https://unlicense.org
+# ---
+
 check_matches_name <- function(data, pattern) {
-  if (!matches_name(data, pattern)) {
+  if (!any(matches_name(data, pattern))) {
     abort(c(
       glue("The data lacks a column matching the pattern '{pattern}'."),
       i = "Are you using the correct data?"
-    ))
+    ), class = "check_matches_name")
   }
   invisible(data)
 }
