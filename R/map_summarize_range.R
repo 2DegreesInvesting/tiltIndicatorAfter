c75b14c25c5c8c486cce2635e5c64be1bffca9e4
Mauro Lepore <maurolepore@gmail.com>
2024-04-18 20:05:08 +0000

    Refactor

--- R/map_summarize_range.R
+++ R/map_summarize_range.R
@@ -1,11 +1,8 @@
 # TODO: Move to tiltIndicator
+# TODO check that .x is a list
+# TODO check that .by is a named list
+# TODO check the relationship between the names of .x and .by
 map_summarize_range <- function(.x, col, .by = NULL, na.rm = FALSE) {
-  .x <- .x
-  # TODO check that .x is a list
-  # TODO check that .by is a named list
-  # TODO check that .by is a named list
-  # TODO check the relationship between the names of .x and .by
-
   out <- vector("list", length = length(.x))
   names(out) <- names(.x)
   for (i in names(.x)) {
