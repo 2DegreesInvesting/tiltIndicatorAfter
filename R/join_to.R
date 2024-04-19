811bac71d8109b4701a5518c3dad7ac637e5cc32
Mauro Lepore <maurolepore@gmail.com>
2024-04-19 18:26:42 +0000

    wip

--- R/join_to.R
+++ R/join_to.R
@@ -12,12 +12,21 @@
 #' @family pipable functions
 #'
 #' @examples
-#' data <- tibble(x = 1:4, y = letters[c(1, 1, 2, 2)])
+#' data <- tibble(x = 1, y = letters[c(1, 2)], z = 1:4)
 #'
 #' data |>
-#'   dplyr::summarise(z = mean(x), .by = "y") |>
+#'   dplyr::summarise(mean = mean(x), .by = "y") |>
 #'   join_to(data)
-join_to <- function(x, y) {
+#'
+#' data |>
+#'   dplyr::summarise(mean = mean(x), .by = "y") |>
+#'   join_to(data, excluding = "z")
+join_to <- function(x, y, excluding = NULL) {
+  if (!is.null(excluding)) {
+    y <- distinct(select(y, -matches(excluding)))
+  }
+
   shared <- intersect(names(x), names(y))
   left_join(y, x, by = shared, relationship = "many-to-many")
 }
+
