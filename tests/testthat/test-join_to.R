07003abe63b9f1385201259e4cd1f5cbccd16ff2
Mauro Lepore <maurolepore@gmail.com>
2024-04-19 17:55:41 +0000

    wip

--- tests/testthat/test-join_to.R
+++ tests/testthat/test-join_to.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("joins as expected", {
+  # styler: off
+  data <- tibble::tribble(
+    ~benchmark, ~emission_profile, ~co2_footprint, ~unit, ~tilt_sector, ~tilt_subsector, ~isic_4digit,
+         "all",             "low",             1L,  "m2",    "sector1",    "subsector1",     "'1234'",
+         "all",             "low",             2L,  "m2",    "sector1",    "subsector1",     "'1234'",
+         "all",            "high",             3L,  "m2",    "sector1",    "subsector1",     "'1234'",
+         "all",            "high",             4L,  "m2",    "sector1",    "subsector1",     "'1234'",
+        "unit",             "low",             1L,  "m2",    "sector1",    "subsector1",     "'1234'",
+        "unit",             "low",             2L,  "m2",    "sector1",    "subsector1",     "'1234'",
+        "unit",            "high",             3L,  "m2",    "sector1",    "subsector1",     "'1234'",
+        "unit",            "high",             4L,  "m2",    "sector1",    "subsector1",     "'1234'",
+  )
+  # styler: off
+
+  summary <- summarize_co2_range(data)
+  expected <- suppressMessages(left_join(data, summary))
+  expect_equal(join_to(summary, data), expected)
 })
