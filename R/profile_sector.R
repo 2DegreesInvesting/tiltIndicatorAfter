13a5061a766bac0dde5c29955e1bccc0b4ad53ba
Mauro Lepore <maurolepore@gmail.com>
2024-04-22 23:34:50 +0000

    Extract profile implementations that we can extend with a composable API

--- R/profile_sector.R
+++ R/profile_sector.R
@@ -9,6 +9,31 @@ profile_sector <- function(companies,
                            isic_tilt = lifecycle::deprecated(),
                            low_threshold = ifelse(scenarios$year == 2030, 1 / 9, 1 / 3),
                            high_threshold = ifelse(scenarios$year == 2030, 2 / 9, 2 / 3)) {
+  profile_sector_impl(
+    companies = companies,
+    scenarios = scenarios,
+    europages_companies = europages_companies,
+    ecoinvent_activities = ecoinvent_activities,
+    ecoinvent_europages = ecoinvent_europages,
+    isic = isic,
+    isic_tilt = isic_tilt,
+    low_threshold = low_threshold,
+    high_threshold = high_threshold
+  )
+}
+
+#' @rdname composable_friends
+#' @export
+#' @keywords internal
+profile_sector_impl <- function(companies,
+                                scenarios,
+                                europages_companies,
+                                ecoinvent_activities,
+                                ecoinvent_europages,
+                                isic,
+                                isic_tilt = lifecycle::deprecated(),
+                                low_threshold = ifelse(scenarios$year == 2030, 1 / 9, 1 / 3),
+                                high_threshold = ifelse(scenarios$year == 2030, 2 / 9, 2 / 3)) {
   if (lifecycle::is_present(isic_tilt)) {
     lifecycle::deprecate_warn(
       "0.0.0.9017",
