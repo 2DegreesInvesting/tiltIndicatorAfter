90a4c6972cd8c90953a4ed113dfdc4d3844dd0be
Mauro Lepore <maurolepore@gmail.com>
2024-04-22 13:02:44 +0000

    Refactor

--- R/exclude.R
+++ R/exclude.R
@@ -1,3 +1,14 @@
+#' Title
+#'
+#' @param data A dataframe.
+#' @param excluding Character vector with patterns to exclude columns and any
+#'   resulting duplicate.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
 exclude <- function(data, excluding) {
   distinct(select(data, -matches(excluding)))
 }
--- R/join_to.R
+++ R/join_to.R
@@ -5,9 +5,6 @@
 #' using a "many-to-many" relationship.
 #'
 #' @inheritParams dplyr::left_join
-#' @param excluding Character vector with patterns to exclude columns via
-#'   [tidyselect::matches()] and any resulting duplicate via
-#'   [dplyr::distinct()].
 #'
 #' @return A data frame with all columns in `x` and `y` and all rows in `y`,
 #'   except the columns and resulting duplicates matched by the argument
@@ -28,7 +25,7 @@
 #'
 #' data |>
 #'   summarize(mean = mean(x), .by = "y") |>
-#'   join_to(data, excluding = "z")
+#'   join_to(data|> excluding("z"))
 join_to <- function(x, y, excluding = NULL) {
   shared <- intersect(names(x), names(y))
   left_join(y, x, by = shared, relationship = "many-to-many")
