f17edc1dc670e7ca9628726526c1be324c2d909d
Mauro Lepore <maurolepore@gmail.com>
2024-04-24 10:47:11 +0000

    Document summarize_co2_range()

--- R/summarize_co2_range.R
+++ R/summarize_co2_range.R
@@ -1,5 +1,44 @@
+#' @keywords internal
+#'
+#' @return A dataframe
 #' @export
-#' @rdname composable_friends
+#' @family composable friends
+#'
+#' @examples
+#' library(readr, warn.conflicts = FALSE)
+#' library(tiltToyData)
+#'
+#' restore <- options(list(
+#'   readr.show_col_types = FALSE,
+#'   tiltIndicatorAfter.output_co2_footprint = TRUE
+#' ))
+#'
+#' companies <- read_csv(toy_emissions_profile_any_companies())
+#' co2 <- read_csv(toy_emissions_profile_products_ecoinvent())
+#' europages_companies <- read_csv(toy_europages_companies())
+#' ecoinvent_activities <- read_csv(toy_ecoinvent_activities())
+#' ecoinvent_europages <- read_csv(toy_ecoinvent_europages())
+#' isic_name <- read_csv(toy_isic_name())
+#'
+#' tilt_profile <- profile_emissions_impl(
+#'   companies,
+#'   co2,
+#'   europages_companies = europages_companies,
+#'   ecoinvent_activities = ecoinvent_activities,
+#'   ecoinvent_europages = ecoinvent_europages,
+#'   isic = isic_name
+#' )
+#'
+#' tilt_profile |>
+#'   summarize_co2_range()
+#'
+#' # Same
+#' tilt_profile |>
+#'   unnest_product() |>
+#'   summarize_co2_range()
+#'
+#' # Cleanup
+#' options(restore)
 summarize_co2_range <- function(data) {
   UseMethod("summarize_co2_range")
 }
