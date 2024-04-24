362d26c110b78d118d925b12db0e67dbabea3064
Mauro Lepore <maurolepore@gmail.com>
2024-04-24 10:43:40 +0000

    document-add_co2_range

--- R/add_co2_range.R
+++ R/add_co2_range.R
@@ -1,4 +1,4 @@
-#' Add co2 range
+#' Add a jittered range of CO2
 #'
 #' `add_co2_range()` is a shortcut for all other `*co2_range()` functions.
 #'
@@ -20,8 +20,6 @@
 #'   tiltIndicatorAfter.output_co2_footprint = TRUE
 #' ))
 #'
-#' # Works with the result of `emissions_profile()` --------------------------
-#'
 #' companies <- read_csv(toy_emissions_profile_any_companies())
 #' co2 <- read_csv(toy_emissions_profile_products_ecoinvent())
 #' europages_companies <- read_csv(toy_europages_companies())
@@ -29,23 +27,19 @@
 #' ecoinvent_europages <- read_csv(toy_ecoinvent_europages())
 #' isic_name <- read_csv(toy_isic_name())
 #'
-#' result <- profile_emissions_impl(
+#' out <- profile_emissions_impl(
 #'   companies,
 #'   co2,
 #'   europages_companies = europages_companies,
 #'   ecoinvent_activities = ecoinvent_activities,
 #'   ecoinvent_europages = ecoinvent_europages,
 #'   isic = isic_name
-#' )
+#' ) |>
+#' add_co2_range()
 #'
-#' # It works with the product-level result
-#' result |>
-#'   unnest_product() |>
-#'   summarize_co2_range()
+#' out |> unnest_product() |> relocate(matches("co2"))
 #'
-#' # But you can conveniently pipe profile result "as is"
-#' result |>
-#'   summarize_co2_range()
+#' out |> unnest_company() |> relocate(matches("co2"))
 #'
 #' # Cleanup
 #' options(restore)
