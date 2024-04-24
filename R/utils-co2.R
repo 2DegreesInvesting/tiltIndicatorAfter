8133a158d461223069ab546b25a7f412121c3014
Mauro Lepore <maurolepore@gmail.com>
2024-04-24 21:25:46 +0000

    Rename

--- R/utils-co2.R
+++ R/utils-co2.R
@@ -6,7 +6,7 @@ create_co2_range <- function(data, amount = set_jitter_amount()) {
     summarize_range(!!ensym(col), .by = all_of(.by)) |>
     jitter_range(amount = amount)
 
-  out |> inform_mean_percent_noise()
+  out |> inform_noise_in_co2_range()
 
   out <- out |> rename(co2e_lower = "min_jitter", co2e_upper = "max_jitter")
 
@@ -20,8 +20,6 @@ create_co2_range <- function(data, amount = set_jitter_amount()) {
 add_co2_upper_lower <- function(data, co2_range) {
   left_join(data, co2_range, by = join_by("grouped_by", "risk_category"))
 }
-
-inform_mean_percent_noise <- inform_noise_in_co2_range
 
 optionally_output_co2_footprint <- function(out, co2_footprint) {
   if (!output_co2_footprint()) {
