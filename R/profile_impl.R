f11ddc808c344bb7f4d61038df2a6b70a4a27102
Mauro Lepore <maurolepore@gmail.com>
2024-04-27 00:53:07 +0000

    Prepare-to-merge

--- R/profile_impl.R
+++ R/profile_impl.R
@@ -1,9 +1,36 @@
-#' Profile implementations
+#' Composable friends
 #'
-#' These functions are designed to work with other functions in the family of
-#' composable friends. Compose them to create complex results.
+#' These functions are designed to work well together. Each does something
+#' small. Compose them to create complex results.
 #'
-#' @family composable friends
-#' @keywords internal
-#' @name profile_impl
+#' @name composable_friends
+#' @examples
+#' library(readr, warn.conflicts = FALSE)
+#' library(tiltToyData)
+#'
+#' restore <- options(list(
+#'   readr.show_col_types = FALSE,
+#'   tiltIndicatorAfter.output_co2_footprint = TRUE
+#' ))
+#'
+#' companies <- read_csv(toy_emissions_profile_any_companies())
+#' co2 <- read_csv(toy_emissions_profile_products_ecoinvent())
+#' europages_companies <- read_csv(toy_europages_companies())
+#' ecoinvent_activities <- read_csv(toy_ecoinvent_activities())
+#' ecoinvent_europages <- read_csv(toy_ecoinvent_europages())
+#' isic_name <- read_csv(toy_isic_name())
+#'
+#' result <- profile_emissions_impl(
+#'   companies,
+#'   co2,
+#'   europages_companies = europages_companies,
+#'   ecoinvent_activities = ecoinvent_activities,
+#'   ecoinvent_europages = ecoinvent_europages,
+#'   isic = isic_name
+#' )
+#'
+#' result
+#'
+#' # Cleanup
+#' options(restore)
 NULL
