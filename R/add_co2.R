6108cb850926206388775b4a2e25849d20624367
Mauro Lepore <maurolepore@gmail.com>
2024-05-03 20:46:35 +0000

    refactor

--- R/add_co2.R
+++ R/add_co2.R
@@ -63,7 +63,7 @@ add_co2.tilt_profile <- function(data,
   summary <- map(.list, function(.x) jitter_co2_range(.x, amount = jitter_amount))
 
   out_product <- summary |>
-    map(~ tiltIndicator:::join_to.data.frame(.x, product)) |>
+    map(function(.x) join_to(.x, product)) |>
     map(~ polish_co2_range.data.frame(.x,
       output_min_max = output_min_max,
       output_co2_footprint = output_co2_footprint
