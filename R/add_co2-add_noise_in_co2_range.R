d2c2102257248606a0788dd77c8e2ed4ab47dd7f
Mauro Lepore <maurolepore@gmail.com>
2024-05-03 16:51:17 +0000

    prefer-separate-pr

--- R/add_co2-add_noise_in_co2_range.R
+++
@@ -1,8 +0,0 @@
-add_noise_in_co2_range <- function(data) {
-  round_mean <- function(x, y) round(mean(percent_noise(x, y), na.rm = TRUE))
-  data |>
-    mutate(
-      min_noise_percent = round_mean(.data$min, .data$min_jitter), na.rm = TRUE,
-      max_noise_percent = round_mean(.data$max, .data$max_jitter), na.rm = TRUE
-    )
-}
