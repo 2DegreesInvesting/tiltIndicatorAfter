62ea4c8e167dbe52f3243398457a8836770a25eb
Mauro Lepore <maurolepore@gmail.com>
2024-05-06 12:18:20 +0000

    Refactor

--- R/polish_emissions_any.R
+++ R/polish_emissions_any.R
@@ -1,15 +1,10 @@
-polish_emissions_any <- function(data, ...) {
-  UseMethod("polish_emissions_any")
-}
-
-#' @export
-polish_emissions_any.tilt_profile <- function(data,
-                                              ...,
-                                              output_min_max = FALSE,
-                                              output_co2_footprint = FALSE) {
+polish_emissions_any <- function(data,
+                                 ...,
+                                 output_min_max = FALSE,
+                                 output_co2_footprint = FALSE) {
   product <- data |>
     unnest_product() |>
-    polish_emissions_any(
+    polish_emissions_any_impl(
       output_min_max = output_min_max,
       output_co2_footprint = output_co2_footprint
     )
@@ -20,11 +15,10 @@ polish_emissions_any.tilt_profile <- function(data,
   tilt_profile(nest_levels(product, company))
 }
 
-#' @export
-polish_emissions_any.data.frame <- function(data,
-                                            ...,
-                                            output_min_max = FALSE,
-                                            output_co2_footprint = FALSE) {
+polish_emissions_any_impl <- function(data,
+                                      ...,
+                                      output_min_max = FALSE,
+                                      output_co2_footprint = FALSE) {
   out <- data |> rename(co2e_lower = "min_jitter", co2e_upper = "max_jitter")
 
   if (!output_min_max) {
