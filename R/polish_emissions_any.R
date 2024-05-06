e6054ca71ae30f72db5237e4c9293b3e0c108165
Mauro Lepore <maurolepore@gmail.com>
2024-05-06 12:00:17 +0000

    Refactor

--- R/polish_emissions_any.R
+++ R/polish_emissions_any.R
@@ -3,24 +3,6 @@ polish_emissions_any <- function(data, ...) {
 }
 
 #' @export
-polish_emissions_any.data.frame <- function(data,
-                                            ...,
-                                            output_min_max = FALSE,
-                                            output_co2_footprint = FALSE) {
-  out <- data |> rename(co2e_lower = "min_jitter", co2e_upper = "max_jitter")
-
-  if (!output_min_max) {
-    out <- out |> exclude(c("^min$", "^max$"))
-  }
-
-  if (!output_co2_footprint) {
-    out <- out |> exclude(col_footprint())
-  }
-
-  out
-}
-
-#' @export
 polish_emissions_any.tilt_profile <- function(data,
                                               ...,
                                               output_min_max = FALSE,
@@ -36,4 +18,22 @@ polish_emissions_any.tilt_profile <- function(data,
     unnest_company()
 
   tilt_profile(nest_levels(product, company))
+}
+
+#' @export
+polish_emissions_any.data.frame <- function(data,
+                                            ...,
+                                            output_min_max = FALSE,
+                                            output_co2_footprint = FALSE) {
+  out <- data |> rename(co2e_lower = "min_jitter", co2e_upper = "max_jitter")
+
+  if (!output_min_max) {
+    out <- out |> exclude(c("^min$", "^max$"))
+  }
+
+  if (!output_co2_footprint) {
+    out <- out |> exclude(col_footprint())
+  }
+
+  out
 }
