797c36f0b52d5c0eba15aaa25ea4b634c89b500c
Mauro Lepore <maurolepore@gmail.com>
2024-05-06 13:27:05 +0000

    Prune

--- R/utils-co2.R
+++ R/utils-co2.R
@@ -19,20 +19,6 @@ add_co2_upper_lower <- function(data, co2_range) {
   left_join(data, co2_range, by = join_by("grouped_by", "risk_category"))
 }
 
-inform_mean_percent_noise <- function(data) {
-  if (!option_verbose()) {
-    return(data)
-  }
-
-  l <- round(mean(percent_noise(data$min, data$min_jitter), na.rm = TRUE))
-  u <- round(mean(percent_noise(data$max, data$max_jitter), na.rm = TRUE))
-  inform(c(i = glue(
-    "Adding {l}% and {u}% noise to `co2e_lower` and `co2e_upper`, respectively."
-  )))
-
-  invisible(data)
-}
-
 optionally_output_co2_footprint <- function(out, co2_footprint) {
   if (!option_output_co2_footprint()) {
     return(out)
