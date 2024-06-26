5fd6cec94b04ce25550bcd30ede2105b66b72ee4
Kalash Singhal <kalash@2degrees-investing.org>
2024-06-26 14:09:55 +0200

    add unit tests

--- R/example_data.R
+++ R/example_data.R
@@ -30,3 +30,17 @@ example_best_case_worst_case_transition_risk_profile_product_level <- example_da
   )
   # styler: on
 )
+
+example_risk_categories_at_product_level <- example_data_factory(
+  # styler: off
+  tribble(
+    ~companies_id, ~ep_product, ~benchmark_tr_score, ~transition_risk_category,
+            "any",         "a",               "all",                     "low",
+            "any",         "a",               "all",                  "medium",
+            "any",         "a",               "all",                    "high",
+            "any",         "b",              "unit",                     "low",
+            "any",         "b",              "unit",                  "medium",
+            "any",         "b",              "unit",                    "high",
+  )
+  # styler: on
+)
